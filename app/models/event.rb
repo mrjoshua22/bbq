260f317640e588b9e775f084e2046f5485cc0200
mr_joshua <ulanov.ng@gmail.com>
2023-01-10 00:51:45 +0300

    Models validations refactor

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
 
   has_many :comments
-
-  validates :user, presence: true
+  has_many :subscriptions
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :address, presence: true
