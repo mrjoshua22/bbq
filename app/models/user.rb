7b15212021a32397d56927ff2b7d84b6b39d8363
mr_joshua <ulanov.ng@gmail.com>
2023-01-12 01:10:07 +0300

    Update subscription if none-user subscriber signs up

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,15 @@ class User < ApplicationRecord
 
   before_validation :set_name, on: :create
 
+  after_commit :link_subscriptions, on: :create
+
   validates :name, presence: true, length: { maximum: 35 }
 
   private
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
+  end
 
   def set_name
     self.name = "Товарищ № #{rand(999)}" if self.name.blank?
