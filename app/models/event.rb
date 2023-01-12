31ee36af8daf1659f42719aa39a5ccda22198f99
mr_joshua <ulanov.ng@gmail.com>
2023-01-12 23:32:38 +0300

    Add destroy dependencies for subscription

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :user
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :subscriptions
   has_many :subscribers, through: :subscriptions, source: :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :subscriptions
 
   before_validation :set_name, on: :create
