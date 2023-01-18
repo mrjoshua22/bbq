e24f831de1c056a2130c3cb6c03a298f9d0bc31c
mr_joshua <ulanov.ng@gmail.com>
2023-01-19 01:08:57 +0300

    Use images from events as event background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,32 @@ module ApplicationHelper
       asset_path('user.png')
     end
   end
+
+  def user_avatar_thumb(user)
+    if user.avatar.attached?
+      user.avatar.variant(:thumb)
+    else
+      asset_path('user.png')
+    end
+  end
+
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      url_for(photos.sample.image.variant(:default))
+    else
+      asset_path('event.jpg')
+    end
+  end
+
+  def event_photo_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      url_for(photos.sample.image.variant(:thumb))
+    else
+      asset_path('event_thumb.jpg')
+    end
+  end
 end
