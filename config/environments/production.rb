15d82603bf5282a25ada824383e87d9f4261cc96
mr_joshua <ulanov.ng@gmail.com>
2023-01-21 01:49:26 +0300

    Fix mail gem argument error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :sendmail
 
+  config.action_mailer.sendmail_settings = {
+    location:  "/usr/sbin/sendmail", arguments: ["-i"]
+  }
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_options = {from: 'no-reply@bbq.mrjoshua22.ru'}
