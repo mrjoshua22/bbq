8a81c5f24c8c6c531273293410234db93859da69
mr_joshua <ulanov.ng@gmail.com>
2023-01-26 23:42:28 +0300

    Config mailer for dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.asset_host = 'http://localhost:3000'
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -69,4 +71,18 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mail.ru',
+    port: '465',
+    enable_starttls_auto: true,
+    tls: true,
+    user_name: ENV["BBQ_MAIL"],
+    password: ENV["BBQ_MAIL_PASSWORD"],
+    authentication: :plain
+  }
 end
