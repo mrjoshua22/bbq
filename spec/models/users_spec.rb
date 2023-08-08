89599c36315aaabbfa7f39f0deea144109b3bb52
mrjoshua22 <ulanov.ng@gmail.com>
2023-08-08 23:45:54 +0300

    Add VK Oauth spec

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -10,7 +10,7 @@ describe User, type: :model do
           provider: 'github',
           info: {
             'email' => 'sample@mail.com',
-            'urls' => { "GitHub"=>"https://github.com/github_user", "Blog"=>"" }
+            'urls' => { 'GitHub'=>'https://github.com/github_user', 'Blog'=>'' }
           }
         )
 
@@ -19,7 +19,7 @@ describe User, type: :model do
 
       context 'when user is not found' do
         let(:user) { User.from_omniauth(access_token) }
-        
+
         it { expect(user).to be_persisted }
         it { expect(user.email).to eq('sample@mail.com') }
         it { expect(user.provider).to eq('github') }
@@ -28,6 +28,34 @@ describe User, type: :model do
 
       context 'when user if found by email' do
         let!(:existing_user) { create(:user, email: 'sample@mail.com') }
+        let!(:some_other_user) { create(:user) }
+
+        it { expect(User.from_omniauth(access_token)).to eq(existing_user) }
+      end
+    end
+
+    context 'when user signs in with vkontakte' do
+      let(:access_token) do
+        access_token = double(:access_token)
+
+        allow(access_token).to receive_messages(
+          provider: 'vkontakte',
+          info: { 'urls' => { 'Vkontakte'=>'https://vk.com/vk_user' }}
+        )
+
+        access_token
+      end
+
+      context 'when user is not found' do
+        let(:user) { User.from_omniauth(access_token) }
+
+        it { expect(user).to be_persisted }
+        it { expect(user.provider).to eq('vkontakte') }
+        it { expect(user.url).to eq('https://vk.com/vk_user') }
+      end
+
+      context 'when user if found by url' do
+        let!(:existing_user) { create(:user, url: 'https://vk.com/vk_user') }
         let!(:some_other_user) { create(:user) }
 
         it { expect(User.from_omniauth(access_token)).to eq(existing_user) }
