37ddb6b34ac43906b684e92ff3b9374d0498376f
mrjoshua22 <ulanov.ng@gmail.com>
2023-08-16 00:50:03 +0300

    Refactor User.from_omniauth spec

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 
 describe User, type: :model do
   describe '.from_omniauth' do
-    context 'when user signs in with github' do
-      let(:access_token) do
-        access_token = double(:access_token)
+    context 'when user signs in' do
+      let(:token_with_email) do
+        token = double(:token_with_email)
 
-        allow(access_token).to receive_messages(
+        allow(token).to receive_messages(
           provider: 'github',
           info: {
             'email' => 'sample@mail.com',
@@ -14,11 +14,22 @@ describe User, type: :model do
           }
         )
 
-        access_token
+        token
+      end
+
+      let(:no_email_token) do
+        token = double(:no_email_token)
+
+        allow(token).to receive_messages(
+          provider: 'vkontakte',
+          info: { 'urls' => { 'Vkontakte'=>'https://vk.com/vk_user' }}
+        )
+
+        token
       end
 
       context 'when user is not found' do
-        let(:user) { User.from_omniauth(access_token) }
+        let(:user) { User.from_omniauth(token_with_email) }
 
         it { expect(user).to be_persisted }
         it { expect(user.email).to eq('sample@mail.com') }
@@ -26,39 +37,18 @@ describe User, type: :model do
         it { expect(user.url).to eq('https://github.com/github_user') }
       end
 
-      context 'when user if found by email' do
+      context 'when user is found by email' do
         let!(:existing_user) { create(:user, email: 'sample@mail.com') }
         let!(:some_other_user) { create(:user) }
 
-        it { expect(User.from_omniauth(access_token)).to eq(existing_user) }
-      end
-    end
-
-    context 'when user signs in with vkontakte' do
-      let(:access_token) do
-        access_token = double(:access_token)
-
-        allow(access_token).to receive_messages(
-          provider: 'vkontakte',
-          info: { 'urls' => { 'Vkontakte'=>'https://vk.com/vk_user' }}
-        )
-
-        access_token
+        it { expect(User.from_omniauth(token_with_email)).to eq(existing_user) }
       end
 
-      context 'when user is not found' do
-        let(:user) { User.from_omniauth(access_token) }
-
-        it { expect(user).to be_persisted }
-        it { expect(user.provider).to eq('vkontakte') }
-        it { expect(user.url).to eq('https://vk.com/vk_user') }
-      end
-
-      context 'when user if found by url' do
+      context 'when user is found by url' do
         let!(:existing_user) { create(:user, url: 'https://vk.com/vk_user') }
         let!(:some_other_user) { create(:user) }
 
-        it { expect(User.from_omniauth(access_token)).to eq(existing_user) }
+        it { expect(User.from_omniauth(no_email_token)).to eq(existing_user) }
       end
     end
   end
